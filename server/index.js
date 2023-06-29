0e81c6eb8fb611c8ab5b701a24ef69e0f755bf72
shivamp2001 <shivampenshnwar2001@gmail.com>
2023-06-29 14:33:03 +0530

    hii

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ mongoose.connect("mongodb+srv://shivamp2001:shivamp2001@mycluster.au9iv5p.mongod
   .then(() => console.log("Database connected successfully"));
 app.use(cors())
 app.use(express.json({limit:"10mb"}))
+app.use("/",(req,res)=>{
+  res.json({message:"working good"})
+})
 
 app.use("/", route);
 app.listen(5000, () => console.log("Server running on port 5000"));
